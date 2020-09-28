54f620fe059a80b13ebdb1b2736d33b83cef9fa5
Julia <yulyakonoval1998@gmail.com>
2020-09-28 17:23:31 +0300

    hover header window created

--- main.js
+++ main.js
@@ -7,10 +7,26 @@ $(document).ready(function () {
 const detailsButton = document.querySelector("#details");
 const sideBar = document.querySelector(".sidebar");
 const closingTag = document.querySelector("#closing-tag");
+const initials = document.querySelector("#initials");
+const hoverHeaderPart = document.querySelector(".header-hover-img");
 
 detailsButton.addEventListener("click", () => {
   sideBar.style.display = "block";
 });
 closingTag.addEventListener("click", () => {
   sideBar.style.display = "none";
+});
+
+$(".round").click(function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+  $(".arrow").toggleClass("bounceAlpha");
+});
+
+initials.addEventListener("mouseenter", () => {
+  hoverHeaderPart.style.display = "block";
+});
+
+initials.addEventListener("mouseover", () => {
+  hoverHeaderPart.style.display = "none";
 });
