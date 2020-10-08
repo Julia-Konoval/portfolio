10a97bdb82da312a416363e8164384400308da85
Julia <yulyakonoval1998@gmail.com>
2020-10-08 13:59:02 +0300

    media query at max-width 550px applied

--- main.js
+++ main.js
@@ -8,14 +8,9 @@ const detailsButton = document.querySelector("#details");
 const sideBar = document.querySelector(".sidebar");
 const closingTag = document.querySelector("#closing-tag");
 const initials = document.querySelector("#initials");
-const hoverHeaderPart = document.querySelector(".header-hover-img");
 const infoButtons = document.querySelectorAll(".info-icon");
 const contactDetails = document.querySelector(".contact-details");
 const closingTagTwo = document.querySelector("#closing-tag-two");
-
-// infoButtons.addEventListener("click", () => {
-//   infoButtons.style.padding === "20px";
-// });
 detailsButton.addEventListener("click", () => {
   sideBar.style.display = "block";
 });
@@ -37,17 +32,6 @@ $(".round").click(function (e) {
   $(".arrow").toggleClass("bounceAlpha");
 });
 
-// hover effect applies to any elements using the 'container' class
-$("#initials").hover(
-  function () {
-    //mouse over
-    hoverHeaderPart.style.display = "block";
-  },
-  function () {
-    //mouse out
-    hoverHeaderPart.style.display = "none";
-  }
-);
 AOS.init({
   easing: "ease",
   duration: 1800,
